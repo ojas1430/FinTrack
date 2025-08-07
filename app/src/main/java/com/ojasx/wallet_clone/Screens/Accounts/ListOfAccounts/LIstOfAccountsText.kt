29c8d9e0e66a0ed8a4ae756f2d334965cbb8f34c
OJAS-CHOUDHARY <ojas1430@gmail.com>
2025-08-07 22:51:09 +0530

    Add ListOfAccounts package

---
+++ app/src/main/java/com/ojasx/wallet_clone/Screens/Accounts/ListOfAccounts/LIstOfAccountsText.kt
@@ -0,0 +1,29 @@
+package com.ojasx.wallet_clone.Screens.Accounts.ListOfAccounts
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ListOfAccountsText() {
+    Spacer(Modifier.width(15.dp))
+    Text(
+        "List of accounts",
+        color = Color.Black,
+        fontSize = 20.sp,
+        fontWeight = FontWeight.Medium,
+
+        )
+}
