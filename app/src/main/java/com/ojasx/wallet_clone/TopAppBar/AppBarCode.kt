3c3ddd83eec684e532bfedf21185896c4451fffb
OJAS-CHOUDHARY <ojas1430@gmail.com>
2025-08-08 10:01:07 +0530

    Add TopAppBar

---
+++ app/src/main/java/com/ojasx/wallet_clone/TopAppBar/AppBarCode.kt
@@ -0,0 +1,64 @@
+package com.ojasx.wallet_clone.TopAppBar
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import com.ojasx.wallet_clone.ui.theme.walletgreen
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppBarCode(modifier: Modifier = Modifier) {
+    TopAppBar(
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = walletgreen,
+            titleContentColor = MaterialTheme.colorScheme.primary,
+        ),
+        navigationIcon = {
+            IconButton(onClick = { }) {
+                Icon(
+                    imageVector = Icons.Filled.Menu,
+                    contentDescription = "Localized description",
+                    tint = Color.White
+
+                    )
+            }
+
+        }, title = {
+            Text(
+                "Home", fontWeight = FontWeight.Bold, color = Color.White
+            )
+        },
+        actions = {
+
+
+            IconButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Filled.Notifications,
+                    contentDescription = "Localized description",
+                    tint = Color.White
+                )
+            }
+
+
+
+        }
+    )
+}
