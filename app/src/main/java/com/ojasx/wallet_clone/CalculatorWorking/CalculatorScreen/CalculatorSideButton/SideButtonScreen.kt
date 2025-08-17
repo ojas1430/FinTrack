0b6f5d94c3ab34c54fd249e8584fdbcb84059bc7
OJAS-CHOUDHARY <ojas1430@gmail.com>
2025-08-17 13:35:10 +0530

    Add SideButton

---
+++ app/src/main/java/com/ojasx/wallet_clone/CalculatorWorking/CalculatorScreen/CalculatorSideButton/SideButtonScreen.kt
@@ -0,0 +1,52 @@
+package com.ojasx.wallet_clone.CalculatorWorking.CalculatorScreen.CalculatorSideButton
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ojasx.wallet_clone.CalculatorWorking.CalculatorScreen.CalculatorSideButton.SideButtonComponents.DateTimePicker
+import com.ojasx.wallet_clone.CalculatorWorking.CalculatorScreen.CalculatorSideButton.SideButtonComponents.LabelSideButton
+import com.ojasx.wallet_clone.CalculatorWorking.CalculatorScreen.CalculatorSideButton.SideButtonComponents.NoteTextField
+import com.ojasx.wallet_clone.CalculatorWorking.CalculatorScreen.CalculatorSideButton.SideButtonComponents.PayeeSideButton
+import com.ojasx.wallet_clone.CalculatorWorking.CalculatorScreen.CalculatorSideButton.SideButtonComponents.TopAppBarSideButton
+import com.ojasx.wallet_clone.StatusBarColor
+
+@Preview
+@Composable
+    fun SideButtonScreen() {
+        // Set status bar color once
+        StatusBarColor()
+
+        Scaffold(
+            topBar = { TopAppBarSideButton() },
+            contentWindowInsets = WindowInsets(0, 0, 0, 0)
+        ) { innerPadding ->
+            LazyColumn(
+                modifier = Modifier.padding(innerPadding)
+            ) {
+                item {
+                    NoteTextField()
+                }
+                item {
+                    LabelSideButton()
+                }
+                item {
+                    PayeeSideButton()
+                }
+                item {
+                    DateTimePicker()
+                }
+            }
+        }
+    }
