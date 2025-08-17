71b1ba2677be16f15bd8ab0982a8a0a514763ab4
OJAS-CHOUDHARY <ojas1430@gmail.com>
2025-08-17 19:57:31 +0530

    Add PlaceSideButton

---
+++ app/src/main/java/com/ojasx/wallet_clone/CalculatorWorking/CalculatorScreen/CalculatorSideButton/SideButtonComponents/PlaceSideButton.kt
@@ -0,0 +1,61 @@
+package com.ojasx.wallet_clone.CalculatorWorking.CalculatorScreen.CalculatorSideButton.SideButtonComponents
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ojasx.wallet_clone.ui.theme.walletblue
+
+@Composable
+fun PlaceSidebutton() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 24.dp, vertical = 8.dp)
+    ) {
+        // Top label
+        Text(
+            text = "Note",
+            color = Color.Gray,
+            fontWeight = FontWeight.Medium
+        )
+
+        // text button
+        TextButton(onClick = {}) {
+            Text(
+                "ADD PLACE",
+                fontSize = 18.sp,
+                color = walletblue
+            )
+        }
+
+        //Underline
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 2.dp)
+                .height(2.dp)
+                .drawBehind {
+                    val strokeWidth = 1.dp.toPx()
+                    val y = size.height - strokeWidth / 2
+                    drawLine(
+                        color = Color.LightGray,
+                        start = Offset(0f, y),
+                        end = Offset(size.width, y),
+                        strokeWidth = strokeWidth
+                    )
+                }
+        )
+    }
+}
