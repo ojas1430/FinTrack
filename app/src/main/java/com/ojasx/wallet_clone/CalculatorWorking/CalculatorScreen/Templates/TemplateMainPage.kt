363552127fb44dff7f3d2936108fd5bd293a145f
OJAS-CHOUDHARY <ojas1430@gmail.com>
2025-08-19 21:39:50 +0530

    Update Template

--- app/src/main/java/com/ojasx/wallet_clone/CalculatorWorking/CalculatorScreen/Templates/TemplateMainPage.kt
+++ app/src/main/java/com/ojasx/wallet_clone/CalculatorWorking/CalculatorScreen/Templates/TemplateMainPage.kt
@@ -1,40 +1,34 @@
 package com.ojasx.wallet_clone.CalculatorWorking.CalculatorScreen.Templates
 
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.ojasx.wallet_clone.CalculatorWorking.CalculatorScreen.CalculatorSideButton.SideButtonComponents.AttachmentsSidebutton
-import com.ojasx.wallet_clone.CalculatorWorking.CalculatorScreen.CalculatorSideButton.SideButtonComponents.DateTimePicker
-import com.ojasx.wallet_clone.CalculatorWorking.CalculatorScreen.CalculatorSideButton.SideButtonComponents.LabelSideButton
-import com.ojasx.wallet_clone.CalculatorWorking.CalculatorScreen.CalculatorSideButton.SideButtonComponents.NoteTextField
-import com.ojasx.wallet_clone.CalculatorWorking.CalculatorScreen.CalculatorSideButton.SideButtonComponents.PayeeSideButton
-import com.ojasx.wallet_clone.CalculatorWorking.CalculatorScreen.CalculatorSideButton.SideButtonComponents.PaymentSidebutton
-import com.ojasx.wallet_clone.CalculatorWorking.CalculatorScreen.CalculatorSideButton.SideButtonComponents.PlaceSidebutton
-import com.ojasx.wallet_clone.CalculatorWorking.CalculatorScreen.CalculatorSideButton.SideButtonComponents.StatusSidebutton
-import com.ojasx.wallet_clone.CalculatorWorking.CalculatorScreen.CalculatorSideButton.SideButtonComponents.TopAppBarSideButton
-import com.ojasx.wallet_clone.CalculatorWorking.CalculatorScreen.CalculatorSideButton.SideButtonComponents.WarrantyPicker
+import com.ojasx.wallet_clone.CalculatorWorking.CalculatorScreen.Category.CardComposable.SubCategoryCards.MainViewModel
+import com.ojasx.wallet_clone.CalculatorWorking.CalculatorScreen.Templates.TemplateComponents.AccountTemplate
 import com.ojasx.wallet_clone.CalculatorWorking.CalculatorScreen.Templates.TemplateComponents.AmountFieldTemplate
+import com.ojasx.wallet_clone.CalculatorWorking.CalculatorScreen.Templates.TemplateComponents.CategoryTemplate
 import com.ojasx.wallet_clone.CalculatorWorking.CalculatorScreen.Templates.TemplateComponents.TemplateName
 import com.ojasx.wallet_clone.CalculatorWorking.CalculatorScreen.Templates.TemplateComponents.TopAppBarTemplate
 import com.ojasx.wallet_clone.StatusBarColor
 import com.ojasx.wallet_clone.ui.theme.walletblue
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.ojasx.wallet_clone.CalculatorWorking.CalculatorScreen.Templates.TemplateComponents.NoteTemplate
+import com.ojasx.wallet_clone.CalculatorWorking.CalculatorScreen.Templates.TemplateComponents.PayeeTemplate
+import com.ojasx.wallet_clone.CalculatorWorking.CalculatorScreen.Templates.TemplateComponents.PaymentTemplate
+import com.ojasx.wallet_clone.CalculatorWorking.CalculatorScreen.Templates.TemplateComponents.PlaceTemplate
+import com.ojasx.wallet_clone.CalculatorWorking.CalculatorScreen.Templates.TemplateComponents.TypeTemplate
+
 
 @Composable
-fun TemplateMainScreen(navController: NavController) {
+fun TemplateMainScreen(navController: NavController){
+    val sharedViewModel: MainViewModel = viewModel()
+
+
     // Set status bar color once
     StatusBarColor(walletblue)
 
@@ -50,6 +44,27 @@ fun TemplateMainScreen(navController: NavController) {
             }
             item {
                 AmountFieldTemplate()
+            }
+            item {
+                AccountTemplate(navController)
+            }
+            item {
+                CategoryTemplate(navController, viewModel = viewModel<MainViewModel>())
+            }
+            item {
+                PaymentTemplate()
+            }
+            item {
+                NoteTemplate()
+            }
+            item {
+                PayeeTemplate()
+            }
+            item {
+                TypeTemplate()
+            }
+            item {
+                PlaceTemplate()
             }
         }
     }
