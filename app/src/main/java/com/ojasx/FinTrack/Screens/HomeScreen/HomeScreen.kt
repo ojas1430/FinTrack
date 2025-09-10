9f20f9220e40e542ad56444737578b828be6a185
OJAS-CHOUDHARY <ojas1430@gmail.com>
2025-09-10 22:33:52 +0530

    Add navigation to sidebar

--- app/src/main/java/com/ojasx/FinTrack/Screens/HomeScreen/HomeScreen.kt
+++ app/src/main/java/com/ojasx/FinTrack/Screens/HomeScreen/HomeScreen.kt
@@ -11,16 +11,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -30,9 +25,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.ojasx.FinTrack.Records.RecordsViewModel
 import com.ojasx.FinTrack.Screens.Accounts.LazyListCards.LazyListOfCards
@@ -43,7 +36,6 @@ import com.ojasx.FinTrack.StatusBarColor
 import com.ojasx.FinTrack.TopAppBar.AppBarButtons
 import com.ojasx.FinTrack.TopAppBar.AppBarCode
 import com.ojasx.FinTrack.ui.theme.walletblue
-import com.ojasx.FinTrack.ui.theme.walletgreen
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -66,11 +58,11 @@ fun HomeScreen(
                 drawerState.close()
                 navController.navigate(route)
             }
-        }
+        },
+        navController = navController
     ) {
         Scaffold(
-            topBar = { AppBarCode(drawerState) }
-            ,
+            topBar = { AppBarCode(drawerState) },
             floatingActionButton = {
                 FloatingActionButton(
                     onClick = { showBottomSheet = true },
