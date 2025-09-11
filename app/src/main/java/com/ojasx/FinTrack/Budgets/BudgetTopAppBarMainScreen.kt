1035143741c86568d7470e5b2d139f8f55a52a63
OJAS-CHOUDHARY <ojas1430@gmail.com>
2025-09-11 19:50:06 +0530

    Add top app bar for budgets screen with 2 buttons

---
+++ app/src/main/java/com/ojasx/FinTrack/Budgets/BudgetTopAppBarMainScreen.kt
@@ -0,0 +1,24 @@
+package com.ojasx.FinTrack.Budgets
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ojasx.FinTrack.TopAppBar.AppBarButtons
+import com.ojasx.FinTrack.TopAppBar.AppBarCode
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun BudgetTopBarMainScreen() {
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        AppBarCode(drawerState)
+        AppBarButtons()
+    }
+}
