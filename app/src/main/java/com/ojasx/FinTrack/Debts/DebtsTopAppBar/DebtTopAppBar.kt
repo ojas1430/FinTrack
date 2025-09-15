b5c218c005179808427d74413e11bf6510f16134
OJAS-CHOUDHARY <ojas1430@gmail.com>
2025-09-15 10:53:28 +0530

    Add DebtTopAppBar.kt

---
+++ app/src/main/java/com/ojasx/FinTrack/Debts/DebtsTopAppBar/DebtTopAppBar.kt
@@ -0,0 +1,46 @@
+package com.ojasx.FinTrack.Debts.DebtsTopAppBar
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.DrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import com.ojasx.FinTrack.StatusBarColor
+import com.ojasx.FinTrack.ui.theme.walletgreen
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DebtTopAppBarCode(drawerState: DrawerState) {
+    val scope = rememberCoroutineScope()
+    StatusBarColor()
+
+    TopAppBar(
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = walletgreen,
+            titleContentColor = MaterialTheme.colorScheme.primary,
+        ),
+        navigationIcon = {
+            IconButton(onClick = { scope.launch { drawerState.open() } }) {
+                Icon(
+                    imageVector = Icons.Filled.Menu,
+                    contentDescription = "Menu",
+                    tint = Color.White
+                )
+            }
+        },
+        title = {
+            Text("Debts", fontWeight = FontWeight.Bold, color = Color.White)
+        },
+
+        )
+}
